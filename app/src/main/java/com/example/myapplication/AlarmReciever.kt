505964130dfe1a68d24f6d215415cbd0648301fa
kingstonwhyte87 <37925345+davdwhyte87@users.noreply.github.com>
2022-12-29 21:08:21 +0100

    alarm manager
    The alarm manager works properly now and time picker

--- app/src/main/java/com/example/myapplication/AlarmReciever.kt
+++ app/src/main/java/com/example/myapplication/AlarmReciever.kt
@@ -4,11 +4,23 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class AlarmReciever : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        // make vibrate
+        val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(3000, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(3000)
+        }
+
+        //make notification
         val i = Intent(context, DestinationActivity::class.java)
         intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val pendingIntent = PendingIntent.getActivity(context, 0,i,0)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.LayoutInflaterCompat
@@ -15,11 +19,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var picker: MaterialTimePicker
+    private  lateinit var calendar: Calendar
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +35,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         createNotificationChannel()
         binding.setTime.setOnClickListener{
-
+            showTimePicker()
         }
+
+        binding.setAlarm.setOnClickListener{
+            setAlarm()
+        }
+        binding.cancelAlarm.setOnClickListener{
+           cancelAlarm()
+        }
+    }
+
+    private fun cancelAlarm(){
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReciever::class.java )
+        pendingIntent = PendingIntent.getBroadcast(this, 0 ,intent, 0)
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(this, "Alarm cancelled", Toast.LENGTH_SHORT).show()
     }
 
     private fun showTimePicker(){
@@ -37,6 +60,30 @@ class MainActivity : AppCompatActivity() {
             .setHour(12)
             .setMinute(0)
             .setTitleText("Select Due Date")
+            .build()
+        picker.show(supportFragmentManager, "AlarmNotify")
+
+        picker.addOnPositiveButtonClickListener{
+            if(picker.hour > 12){
+                binding.selectedTime.text= String.format("%02d", picker.hour-12)+" : " +String.format("%02d", picker.minute)+" PM"
+            }else{
+                binding.selectedTime.text= String.format("%02d", picker.hour-12)+" : " +String.format("%02d", picker.minute)+" AM"
+            }
+
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY]= picker.hour
+            calendar[Calendar.MINUTE] = picker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND]= 0
+        }
+    }
+
+    private fun setAlarm(){
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReciever::class.java )
+        pendingIntent = PendingIntent.getBroadcast(this, 0 ,intent, 0)
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+        Toast.makeText(this, "Alarm set successfully", Toast.LENGTH_SHORT).show()
     }
 
     private fun createNotificationChannel(){
